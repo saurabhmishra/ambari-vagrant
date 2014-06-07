997800b7e1adaf64f7515936001765564a009d1e
Saurabh Mishra <saurabh_mishra@symantec.com>
2014-06-06 18:10:23 -0700

    removed other packages

--- ambari-vagrant/centos5.9/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cp /vagrant/hosts /etc/hosts
-cp /vagrant/resolv.conf /etc/resolv.conf
-yum install iptables -y
-cp /vagrant/epel.repo /etc/yum.repos.d
-service ntpd start
-mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
-
--- ambari-vagrant/centos6.4/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cp /vagrant/hosts /etc/hosts
-cp /vagrant/resolv.conf /etc/resolv.conf
-yum install ntp -y
-service ntpd start
-service iptables stop
-mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
