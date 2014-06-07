a07b6d3714e031cb481b57547d450b2a2f3b6637
Saurabh Mishra <saurabh_mishra@symantec.com>
2014-06-06 17:47:41 -0700

    ambari vagrant scripts

---
+++ ambari-vagrant/centos6.5/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cp /vagrant/hosts /etc/hosts
+cp /vagrant/resolv.conf /etc/resolv.conf
+yum -y install wget
+yum -y install mysql-server
+yum install ntp -y
+service ntpd start
+service iptables stop
+service mysqld start
+mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
+wget -q http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.5.1/ambari.repo -O /etc/yum.repos.d/ambari.repo
+yum -q clean all
+yum -q repolist
+yum -q -y install ambari-server ambari-agent
+/etc/init.d/ambari-server setup -s
+wget --no-check-certificate https://raw.githubusercontent.com/saurabhmishra/Ambari/master/Ambari/ambari.properties  -O /etc/ambari-server/conf/ambari.properties
+/etc/init.d/ambari-server start
+/etc/init.d/ambari-agent start
+sleep 20
+wget --no-check-certificate https://raw.githubusercontent.com/saurabhmishra/Ambari/master/Ambari/ambari_auto.sh -O /tmp/ambari_auto.sh
+sh /tmp/ambari_auto.sh
